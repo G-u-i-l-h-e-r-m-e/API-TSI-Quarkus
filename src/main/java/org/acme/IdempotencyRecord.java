0c4b3e0f684b88aeb19db1acb2946fa334d89972
GUILHERME ANDRADE SOUZA <guilherme.asouza29@senacsp.edu.br>
2025-04-22 21:38:35 -0300

    correção idempotencia

--- src/main/java/org/acme/IdempotencyRecord.java
+++ src/main/java/org/acme/IdempotencyRecord.java
@@ -2,7 +2,7 @@ package org.acme;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
-
+@Entity
 public class IdempotencyRecord extends PanacheEntity{
     public String keyRecord;
 }
